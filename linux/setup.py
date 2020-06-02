54e9fbb6a0e26d2cc7d29d5f6be7637841c835cd
Brent Jameson <noreply@jamesonnetworks.com>
2020-06-02 18:44:57 -0400

    Add change shell to linux setup

--- linux/setup.py
+++ linux/setup.py
@@ -1,3 +1,5 @@
+import os
 
 def main(args):
 	print("Running Linux specific environment setup...")
+    os.system('chsh zsh')
--- linux/setup.sh
+++ linux/setup.sh
@@ -1,5 +1,4 @@
 apt install zsh
-chsh -s /usr/bin/zsh
-apt install build-essential cmake vim python3-dev
+apt install build-essential cmake vim python3-dev golang
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py --all
