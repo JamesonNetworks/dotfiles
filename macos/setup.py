1295437ea08f0ef802c24b719dd85ade2f876b2b
Brent Jameson <878987+JamesonNetworks@users.noreply.github.com>
2020-06-05 10:33:35 -0400

    Add reminder to install powerline fonts

--- macos/setup.py
+++ macos/setup.py
@@ -1,8 +1,16 @@
 from shutil import copyfile
 from pathlib import PosixPath
+import os
+
+
+def installFonts(args):
+    print("Install PowerLine Fonts from: https://github.com/powerline/fonts")
+    
 
 
 def main(args):
     print("Running MacOS specific environment setup...")
     print("Overwriting zshrc")
     copyfile(PosixPath("./macos/zshrc"), PosixPath("~/.zshrc").expanduser())
+    installFonts(args=args)
+
